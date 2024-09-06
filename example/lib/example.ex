7460a57c0711877019855c3355564cbdd0a30460
Dulina-Perera <dulina.perera@outlook.com>
2024-09-06 08:10:42 +0530

    Learnt how to work with basic variables and module attributes.

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,14 +1,16 @@
 defmodule Example do
   use Application
 
-  alias UUID
+  @x 5 # Bind x to 5. x is a compile-time attribute.
 
   def start(_type, _args) do
-    IO.puts(UUID.uuid4())
+    Example.main()
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
-  def hello do
-    :world
+  def main do
+    x = 9 # Bind x to 9. x is a runtime variable.
+    x = 6 # Rebind x to 6. x is a runtime variable.
+    IO.puts(x + @x) # 6 + 5 = 11
   end
 end
