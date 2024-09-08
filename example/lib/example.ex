60ab0044a23e9f6f44f562a0263dda6e5d75acc2
Dulina-Perera <dulina.perera@outlook.com>
2024-09-08 21:08:41 +0530

    Wrote a simple program to print how much time is left before the new year.

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -7,11 +7,16 @@ defmodule Example do
   end
 
   def main do
-    date = Date.new!(2024, 09, 08)
-    time = Time.new!(18, 58, 32)
-    datetime = DateTime.new!(date, time, "Etc/UTC")
+    next_year = DateTime.new!(Date.new!(Date.utc_today().year + 1, 1, 1), Time.new!(0, 0, 0, 0), "Etc/UTC")
 
-    IO.inspect(datetime)
-    IO.puts(datetime.year)
+    secs_till_next_year = DateTime.diff(next_year, DateTime.utc_now(), :second)
+    days_till_next_year = div(secs_till_next_year, 86400)
+    hours = div(rem(secs_till_next_year, 86400), 3600)
+    minutes = div(rem(secs_till_next_year, 3600), 60)
+    seconds = rem(secs_till_next_year, 60)
+
+    IO.puts(
+      "New year is #{days_till_next_year} days and #{hours} hours and #{minutes} minutes and #{seconds} seconds away."
+    )
   end
 end
